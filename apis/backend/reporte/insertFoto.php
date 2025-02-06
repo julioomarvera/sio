bd687f51d8005a09a0e40b2e6dd77ee472c34954
Jesús <jesusfhohs@gmail.com>
2025-02-06 13:06:59 -0600

    feat: :fire: agregando carga de foto con registro en documentos (falta cargar la imaagen en SIO WEB)

---
+++ apis/backend/reporte/insertFoto.php
@@ -0,0 +1,102 @@
+<?php
+$dir_fc = "../";
+include_once $dir_fc.'data/reportes.class.php';	
+require_once $dir_fc."common/function.class.php";	
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Firebase\JWT\JWT;
+
+$app->post('/reporte/insertFoto',function(Request $request, Response $response){
+
+    $id_reporte = $request->getParam('id_reporte');
+    $id_historia_reporte = $request->getParam('id_seguimiento');
+    $id_usuario_captura = $request->getParam('id_usuario');
+    $uploadedFiles = $request->getUploadedFiles();
+
+	$cFn 	 = new cFunction();
+	$cAccion = new cReports();
+
+	$headers = $request->getHeaders();
+
+	class mensaje {
+		public $done;
+		public $msg;
+		public $id;
+	}
+
+	try{
+
+		$done	 = false;
+		$msg     = "";
+		$insertDocumentoDB  = 0;
+
+		$token 	 = $cFn->getToken( $headers );
+
+		if($token == ""){
+			throw new Exception("No token available");
+		}
+        
+        JWT::decode($token, _SECRET_JWT_, array('HS256')); //valida jwt, si no es válido tira una exepción
+
+        if($id_reporte == "" || !is_numeric($id_reporte)){
+            throw new Exception("Se requiere del número de reporte");
+        }
+
+        if($id_usuario_captura == "" || !is_numeric($id_usuario_captura)){
+            throw new Exception("Debes de especificar el usuario que está realizando el moviemiento");
+        }
+
+        $pathLocal = "img/fotoseguimiento/";
+        $uploadDir = "../$pathLocal"; 
+
+        $uploadedFile = $uploadedFiles['foto'];
+
+        if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+            $permitidos = array("image/jpg", "image/jpeg", "image/png");
+            
+            if(in_array($uploadedFile->getClientMediaType(), $permitidos)){
+                $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+    
+                $basename = bin2hex(random_bytes(8));
+                $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+                $dataInsertDocto = array(
+                    $id_reporte,
+                    $id_historia_reporte,
+                    $id_usuario_captura,
+                    $filename,
+                    $extension,
+                    $pathLocal,
+                );
+        
+                $insertDocumentoDB = $cAccion->insertDocumento($dataInsertDocto);
+    
+                $uploadedFile->moveTo($uploadDir . DIRECTORY_SEPARATOR . $filename);
+            }else{
+                throw new Exception("El tipo de dato adjunto no es válido");
+            }
+        }
+
+        if(!is_numeric($insertDocumentoDB )){
+            throw new Exception("Ocurrió un inconveniente ".$insertDocumentoDB);
+        }
+
+        $done = true;
+        $msg = "Registro insertado correctamente";
+
+		$resp = new mensaje();
+		$resp->done = $done;
+		$resp->msg  = $msg;
+		$resp->id   = $insertDocumentoDB;
+
+		return $response->withJson($resp,200);
+
+
+	}catch(Exception $e){
+		$resp = new mensaje();
+		$resp->done = false;
+		$resp->msg 	 = "Error: ". $e->getMessage();
+		return $response->withJson($resp,400);
+	}	   	   
+});
---
+++ database/todo/04_JF_lon_lat_historia.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tbl_reporte_historia ADD latitud varchar(100) DEFAULT null NULL;
+ALTER TABLE tbl_reporte_historia ADD longitud varchar(100) DEFAULT null NULL;
