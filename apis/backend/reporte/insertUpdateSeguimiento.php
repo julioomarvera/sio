c7885e35f40c1dc1d56bd9ac74f07d50fe827a5a
Jesús <jesusfhohs@gmail.com>
2025-02-11 19:45:58 -0600

    feat: :art: mover la imagen al sio en qa para que se muestre directo allí

--- apis/backend/reporte/insertUpdateSeguimiento.php
+++ apis/backend/reporte/insertUpdateSeguimiento.php
@@ -136,7 +136,7 @@ $app->post('/reporte/insertupdateSeguimiento',function(Request $request, Respons
             $new_route = "../".$pathLocal.$pathLocalName;
             $route_ant = "../".$pathLocalAnt.$uploadedFiles;
 
-            $sio_dir = "../../atencionc/files/reporte/historia";
+            $sio_dir = "../../atencionc/files/reporte/historia"; //este despues se agrega a variable de entorno
 
             $rutaCopyToSio = $sio_dir."/".$pathLocalName;
             //$uploadedFile->moveTo($uploadDir . DIRECTORY_SEPARATOR . $filename);
